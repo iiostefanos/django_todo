1deabe04b1482c9c8b566db22d19317fc6361346
StefanoS <iiostefanos@gmail.com>
2020-05-23 10:00:54 +0000

    rendering data

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import Item
+
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello World")
+def get_todo_list(request):
+    results = Item.objects.all()
+    return render(request, "todo_list.html", {
+        'items': results
+    })
